898b542c7195c7ea7398ff819f00f7dcb49ce231
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 18:55:56 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 import os
 
 # Ganti dengan URL dan Anon Key dari Supabase kamu
-SUPABASE_URL = https://supabase.com/dashboard/project/nwsjgtrzyrebdaioczhj/settings/api-keys
+SUPABASE_URL = https://supabase.com/dashboard/project/nwsjgtrzyrebdaioczhj
 SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
