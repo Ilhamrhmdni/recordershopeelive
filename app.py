9224852d6255e7d2e2f997cac87f73e460dac210
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-08 01:35:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import pandas as pd
 
 # Konfigurasi Supabase
-url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_KEY"]
+url = "https://nwsjgtrzyrebdaioczhj.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw"
 supabase: Client = create_client(url, key)
 
 # Fungsi validasi
