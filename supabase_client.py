1710f97e9a7afa637dd1bfaa03b4ad504a821be2
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 22:29:35 +0700

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 @st.cache_resource
 def init_supabase():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["https://nwsjgtrzyrebdaioczhj.supabase.co"]
+    key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw"]
     return create_client(url, key)
 
 supabase = init_supabase()
