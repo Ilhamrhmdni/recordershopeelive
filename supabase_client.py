493b06022acff65f993369717216af2628b15a21
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 23:06:04 +0700

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,10 +1,15 @@
+# supabase_client.py
 from supabase import create_client, Client
-import streamlit as st
+import os
+from dotenv import load_dotenv
 
-@st.cache_resource
-def init_supabase():
-    url = st.secrets="https://nwsjgtrzyrebdaioczhj.supabase.co"
-    key = st.secrets="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw"
-    return create_client(url, key)
+load_dotenv()  # Load .env file jika kamu menyimpan API key di sana
 
-supabase = init_supabase()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+def get_supabase_client() -> Client:
+    if not SUPABASE_URL or not SUPABASE_KEY:
+        raise Exception("Supabase URL atau KEY belum diatur di environment variables.")
+    
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
