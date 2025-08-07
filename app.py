cd52049f973f275c1062c5939d77dc1d51db5ebc
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 22:56:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from datetime import datetime
 # Inisialisasi Supabase
 @st.cache_resource
 def init_supabase():
-    url = st.secrets="supabase_url"
-    key = st.secrets="supabase_key"
+    url = st.secrets="https://nwsjgtrzyrebdaioczhj.supabase.co"
+    key = st.secrets="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw"
     return create_client(url, key)
 
 supabase = init_supabase()
