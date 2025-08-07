6edaf8a2ebf8510e169d06e2efb75e547a89f821
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 22:26:13 +0700

    Create supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,10 @@
+from supabase import create_client, Client
+import streamlit as st
+
+@st.cache_resource
+def init_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_supabase()
