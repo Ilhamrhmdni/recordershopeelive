528e405f18e7503b1382b326b8d5ce404118034e
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-08-07 23:08:40 +0700

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load .env file jika kamu menyimpan API key di sana
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv"https://nwsjgtrzyrebdaioczhj.supabase.co"
+SUPABASE_KEY = os.getenv"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2pndHJ6eXJlYmRhaW9jemhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjM4ODgsImV4cCI6MjA3MDEzOTg4OH0.jxmfTFHp7vSwkzE5dVgw7-WKdgKUTxIsysaeA9DRSCw"
 
 def get_supabase_client() -> Client:
     if not SUPABASE_URL or not SUPABASE_KEY:
